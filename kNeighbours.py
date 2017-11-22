96291b7485c06a928a2af4aded3413d48858c4d4
stosik <tosikszymon@gmail.com>
2017-11-22 08:24:49 +0100

    Added reponse

--- kNeighbours.py
+++ kNeighbours.py
@@ -2,6 +2,7 @@ import csv
 import random
 import math
 import operator
+
 def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	with open(filename, 'r') as csvfile:
 	    lines = csv.reader(csvfile)
@@ -46,8 +47,17 @@ def getNeighbors(distanceMethod, trainingSet, testInstance, k):
 		neighbors.append(distances[x][0])
 	return neighbors
 
-trainSet = [[2,2,2,'a'], [4,4,4,'b']]
-testInstance = [5,5,5]
-k = 1
-neighbors = getNeighbors(manhattanDistance, trainSet, testInstance, 1)
-print(neighbors)
+def getResponse(neighbors):
+	classVotes = {}
+	for x in range(len(neighbors)):
+		response = neighbors[x][-1]
+		if response in classVotes:
+			classVotes[response] += 1
+		else:
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+	return sortedVotes[0][0]
+
+neighbors = [[1,1,1,'a'], [2,2,2,'a'], [3,3,3,'b']]
+response = getResponse(neighbors)
+print(response)
